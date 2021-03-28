a08e82cd852a61f50646a78710eac78d74d7c72f
Eslam El-feky <33692485+eslamira@users.noreply.github.com>
2021-03-28 22:08:33 +0200

    fixing syntax after upgrading pubspec.yaml

--- lib/src/manager_dio.dart
+++ lib/src/manager_dio.dart
@@ -38,7 +38,7 @@ class DioCacheManager {
     return _interceptor;
   }
 
-  _onRequest(RequestOptions options) async {
+  _onRequest(RequestOptions options, handler) async {
     if ((options.extra[DIO_CACHE_KEY_TRY_CACHE] ?? false) != true) {
       return options;
     }
@@ -53,8 +53,8 @@ class DioCacheManager {
     return options;
   }
 
-  _onResponse(Response response) async {
-    if ((response.request.extra[DIO_CACHE_KEY_TRY_CACHE] ?? false) == true &&
+  _onResponse(Response response, handler) async {
+    if ((response.extra[DIO_CACHE_KEY_TRY_CACHE] ?? false) == true &&
         response.statusCode >= 200 &&
         response.statusCode < 300) {
       await _pushToCache(response);
@@ -62,12 +62,13 @@ class DioCacheManager {
     return response;
   }
 
-  _onError(DioError e) async {
-    if ((e.request.extra[DIO_CACHE_KEY_TRY_CACHE] ?? false) == true) {
-      var responseDataFromCache = await _pullFromCacheBeforeMaxStale(e.request);
+  _onError(DioError e, handler) async {
+    if ((e.response.extra[DIO_CACHE_KEY_TRY_CACHE] ?? false) == true) {
+      var responseDataFromCache =
+          await _pullFromCacheBeforeMaxStale(e.requestOptions);
       if (null != responseDataFromCache)
         return _buildResponse(responseDataFromCache,
-            responseDataFromCache?.statusCode, e.request);
+            responseDataFromCache?.statusCode, e.requestOptions);
     }
     return e;
   }
@@ -94,7 +95,8 @@ class DioCacheManager {
         data: data,
         headers: headers,
         extra: options.extra..remove(DIO_CACHE_KEY_TRY_CACHE),
-        statusCode: statusCode ?? 200);
+        statusCode: statusCode ?? 200,
+        requestOptions: options);
   }
 
   Future<CacheObj> _pullFromCacheBeforeMaxAge(RequestOptions options) {
@@ -110,7 +112,7 @@ class DioCacheManager {
   }
 
   Future<bool> _pushToCache(Response response) {
-    RequestOptions options = response.request;
+    RequestOptions options = response.requestOptions;
     Duration maxAge = options.extra[DIO_CACHE_KEY_MAX_AGE];
     Duration maxStale = options.extra[DIO_CACHE_KEY_MAX_STALE];
     if (null == maxAge) {
